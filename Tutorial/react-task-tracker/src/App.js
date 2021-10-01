39f543fb47c0c29eab95a3e14459d3fba62d5412
Sophie <tengxxu@gmail.com>
2021-09-30 22:24:37 -0400

    Add react task tracker

--- Tutorial/react-task-tracker/src/App.js
+++ Tutorial/react-task-tracker/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+const name = 'Xiangxu'
+
   return (
+    // whatever you returns has to be a single element
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
     </div>
   );
 }
