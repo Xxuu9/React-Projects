cdafc69eab73ae5bf51cc599c25bf67e9d04aa83
Sophie <tengxxu@gmail.com>
2021-10-01 13:34:02 -0400

    add inline style

--- Tutorial/react-task-tracker/src/components/Header.js
+++ Tutorial/react-task-tracker/src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
     return (
         <header>
-            <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
+            <h1 style={headingStyle}>{title}</h1>
         </header>
     )
 }
@@ -17,8 +17,9 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-// const headingStyle = {
-
-// }
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black',
+}
 
 export default Header
