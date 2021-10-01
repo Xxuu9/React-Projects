bf3c48371f69c111615b9543a42e4ed10a38b964
Sophie <tengxxu@gmail.com>
2021-10-01 11:58:22 -0400

    Add react project

--- Tutorial/react-task-tracker/src/App.js
+++ Tutorial/react-task-tracker/src/App.js
@@ -1,11 +1,13 @@
 function App() {
 const name = 'Xiangxu'
+const x = true
 
   return (
     // whatever you returns has to be a single element
     <div className="App">
       <h1>Hello From React</h1>
       <h2>Hello {name}</h2>
+      <h2>Hello {x ? 'Yes': 'No'}</h2>
     </div>
   );
 }
