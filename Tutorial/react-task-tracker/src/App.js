4010f4491a3ecd6c09ad703a8ea02f52acabd478
Sophie <tengxxu@gmail.com>
2021-10-01 13:27:39 -0400

    add inline style

--- Tutorial/react-task-tracker/src/App.js
+++ Tutorial/react-task-tracker/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Header from './components/Header'
 
-// function App() {
-//   return (
-//     // whatever you returns has to be a single element
-//     <div className="container">
-//       <Header />
-//     </div>
-//   );
-// }
-
-class App extends React.Component{
-  render(){
-    return <h1>Hello from a class</h1>
-  }
+function App() {
+  return (
+    // whatever you returns has to be a single element
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
+
+
 
 export default App;
--- Tutorial/react-task-tracker/src/components/Header.js
+++ Tutorial/react-task-tracker/src/components/Header.js
@@ -1,9 +1,24 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+
+const Header = ({title}) => {
     return (
         <header>
-            
+            <h1 style={{color: 'red', backgroundColor: 'black'}}>{title}</h1>
         </header>
     )
 }
+
+Header.defaultProps ={
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// const headingStyle = {
+
+// }
 
 export default Header
