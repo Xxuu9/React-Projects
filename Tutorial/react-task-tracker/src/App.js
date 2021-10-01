9d6ffd22ce5a3107454ad685e427929f226268e0
Sophie <tengxxu@gmail.com>
2021-10-01 12:28:38 -0400

    using a class

--- Tutorial/react-task-tracker/src/App.js
+++ Tutorial/react-task-tracker/src/App.js
@@ -1,15 +1,19 @@
-function App() {
-const name = 'Xiangxu'
-const x = true
+import React from 'react';
+import Header from './components/Header'
 
-  return (
-    // whatever you returns has to be a single element
-    <div className="App">
-      <h1>Hello From React</h1>
-      <h2>Hello {name}</h2>
-      <h2>Hello {x ? 'Yes': 'No'}</h2>
-    </div>
-  );
+// function App() {
+//   return (
+//     // whatever you returns has to be a single element
+//     <div className="container">
+//       <Header />
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    return <h1>Hello from a class</h1>
+  }
 }
 
 export default App;
